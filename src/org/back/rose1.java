2da8c3718e45be2ab90543fc1ec6c9c1b1986f0b
kumarstark <sathish4live@gmail.com>
2022-02-27 03:20:19 -0500

    i am done

---
+++ src/org/back/rose1.java
@@ -0,0 +1,17 @@
+package org.back;
+
+public class rose1 {
+	public static void main(String[] args) {
+		System.out.println("rs 78");
+		System.out.println("rs2879");
+		System.out.println("rs38524");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
